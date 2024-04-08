9785cf7c97f2a44221c46748ff501fe7af8df676
Anahit Sargsyan <asargsya@c1r3s6.42yerevan.am>
2024-04-08 15:28:13 +0400

    getting started

---
+++ main.py
@@ -0,0 +1,39 @@
+'''
+ADD, UPDATE, LIST, REMOVE, EXIT.
+in order to make the code easier to understand, I'm going to write a seperate 
+function for each of the commands, then call them in the main() function 
+'''
+
+#function to add a task, as a parameter i'm passing the list of commands
+#we already have 
+def addTask(commands):
+    #prompts for the name of the task
+    nameTask = input("Enter the name of the task you want to add: ")
+    #check if the task exists already, if so, suggest updating the description
+    if(command)
+
+def updateTask():
+
+def listTasks():
+
+def removeTask():
+
+
+
+def main():
+
+    '''
+    commands will looks like this
+    [
+        {
+            "name" : task_name,
+            "description" : task_desc
+        },
+        {
+            "name": task_name2.
+            "description" : task_desc2
+
+        }
+    ]
+    '''
+    commands = [{}]
